8aa202d7e5fc612249f63cb02dd49ef0a3f6b255
zwang <alexwang119@yahoo.com>
2021-04-06 15:14:30 +0800

    Update Eureka multiple reg Services

---
+++ eureka-security-server/src/main/java/com/zwang/eureka/security/server/eurekasecurityserver/EurekaSecurityServerApplication.java
@@ -0,0 +1,13 @@
+package com.zwang.eureka.security.server.eurekasecurityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSecurityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaSecurityServerApplication.class, args);
+    }
+
+}
